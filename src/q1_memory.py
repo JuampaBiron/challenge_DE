827b73324921d14a8e3a2fd7de92334737e17c07
juampa biron <85587142+JuampaBiron@users.noreply.github.com>
2024-10-18 20:42:46 -0300

    uploaded files

---
+++ src/q1_memory.py
@@ -0,0 +1,15 @@
+from typing import List, Tuple
+from datetime import datetime
+"""Las top 10 fechas donde hay más tweets. Mencionar el usuario (username) que más publicaciones tiene por cada uno de esos días. Debe incluir las siguientes funciones:
+```python
+def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
+```
+```python
+def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
+```
+```python
+Returns: 
+[(datetime.date(1999, 11, 15), "LATAM321"), (datetime.date(1999, 7, 15), "LATAM_CHI"), ...]
+```"""
+def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
+    pass
